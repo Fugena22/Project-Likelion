601073901d676e0772cadd13c03be07c083ec607
Nguyen Tuan Phuc <acediawalker@gmail.com>
2023-01-12 14:45:06 +0700

    md

--- src/components/Overview/Overviews.jsx
+++ src/components/Overview/Overviews.jsx
@@ -8,24 +8,24 @@ const Overviews = () => {
   return (
     <div className="flex flex-row justify-between px-8">
       <div className="flex flex-col items-center p-0 border-0.5 border-solid border-[#939393] rounded">
-        <div className="self-stretch border-b-0.5 border-b-[#939393] border-solid">
-          <span className="font-text text-base flex items-center tracking-tight text-black">
-            Tổng quan
+        <div className="self-stretch border-b-0.5 border-b-[#939393] border-solid relative w-36 h-11">
+          <span className="font-text text-base flex items-center tracking-tight text-black absolute top-2 left-6">
+            <a href="#question">Tổng quan</a>
           </span>
         </div>
-        <div className="self-stretch border-b-0.5 border-b-[#939393] border-solid">
-          <span className="font-text text-base flex items-center tracking-tight text-black">
-            Khoá học
+        <div className="self-stretch border-b-0.5 border-b-[#939393] border-solid relative w-36 h-11">
+          <span className="font-text text-base flex items-center tracking-tight text-black absolute top-2 left-6">
+            <a href="#courses">Khoá học</a>
           </span>
         </div>
-        <div className="self-stretch border-b-0.5 border-b-[#939393] border-solid">
-          <span className="font-text text-base flex items-center tracking-tight text-black">
-            Học phí
+        <div className="self-stretch border-b-0.5 border-b-[#939393] border-solid relative w-36 h-11">
+          <span className="font-text text-base flex items-center tracking-tight text-black absolute top-2 left-6">
+            <a href="#charge">Học phí</a>
           </span>
         </div>
-        <div className="self-stretch border-b-0.5 border-b-[#939393] border-solid">
-          <span className="font-text text-base flex items-center tracking-tight text-black">
-            Lập trình
+        <div className="self-stretch border-b-0.5 border-b-[#939393] border-solid relative w-36 h-11">
+          <span className="font-text text-base flex items-center tracking-tight text-black absolute top-2 left-6">
+            <a href="#coding">Lập trình</a>
           </span>
         </div>
       </div>
